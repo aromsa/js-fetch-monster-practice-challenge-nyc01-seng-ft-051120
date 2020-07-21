ae544d9403c5fb8b74f9d476ccdc4fbf6aab7f8d
aromsa <annacbateman@gmail.com>
2020-07-21 17:56:03 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,21 +1,48 @@
 document.addEventListener("DOMContentLoaded", () => {
   // VARIABLES
   const allMonsters = "http://localhost:3000/monsters"
-  const fiftyMonsters = "http://localhost:3000/monsters/?_limit=50&_page=3"
+  const fiftyMonsters = "http://localhost:3000/monsters/?_limit=50&_page=1"
   const monsterContainer = document.getElementById('monster-container')
   const createMonsterDiv = document.getElementById('create-monster')
-  const monsterForm = document.getElementById('monster-form')
-  console.log(monsterForm)
+  let pageCounter = 1 //why do I have to call this outside the function? 
 
   // FETCH REQUESTS
-  function fetchMonsters(){
-    fetch(fiftyMonsters)
+  function fetchMonsters(url){
+    fetch(url)
     .then(resp => resp.json())
     .then(monsters => renderMonsters(monsters))
   }
 
-  // EVENT LISTENERS
+  function createNewMonster(newMonster){
+    fetch(allMonsters, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "accept": "application/json"
+      },
+      body: JSON.stringify(newMonster)
+    })
+    alert(`${newMonster.name} has been saved.`)
+  }
 
+  // EVENT LISTENERS
+  document.addEventListener("click",function(e){
+      if (e.target.id === "forward"){
+          pageCounter += 1            
+          monsterContainer.innerHTML=""
+          fetchMonsters(`http://localhost:3000/monsters/?_limit=50&_page=${pageCounter}`)
+      }
+      else if (e.target.id === "back"){
+          if (pageCounter>1) {
+              pageCounter -= 1           
+              monsterContainer.innerHTML=""
+              fetchMonsters(`http://localhost:3000/monsters/?_limit=50&_page=${pageCounter}`)
+          }
+          else {
+              alert('You are current on page 1.')
+          }
+      }
+  })
 
 
   // FUNCTIONS
@@ -42,13 +69,22 @@ document.addEventListener("DOMContentLoaded", () => {
       <input type="text" id="age" name="age">
       <label for="description">description:</label>
       <input type="text" id="description" name="description">
-      <button type="button">Create Monster</button>
+      <button type="submit">Create Monster</button>
     `
     createMonsterDiv.append(monsterForm)
+    monsterForm.addEventListener("submit", e => {
+      e.preventDefault()
+      let newMonsterForm = e.target
+      let name = e.target.name.value
+      let age = e.target.age.value
+      let description = e.target.description.value
+      let newMonster = {name, age, description}
+      createNewMonster(newMonster)
+      newMonsterForm.reset()
+    })
   }
 
   // EXECUTIONS
-  fetchMonsters()
+  fetchMonsters(fiftyMonsters)
   createMonsterForm()
-
 })
